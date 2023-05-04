320a4271e6672791dd0aad69615144fbfa36afbf
abe3333 <62336021+abe3333@users.noreply.github.com>
2023-05-04 20:27:04 +0900

    S3からのファイルインポート機能

---
+++ SlackExport/Common/HttpAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackExport.Common
+{
+    public class HttpAccess
+    {
+        public string get(string url, string token)
+        {
+            string responseBody = string.Empty;
+
+            string bearerToken = "Bearer " + " " + token;
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", bearerToken);
+                using (HttpResponseMessage response = client.GetAsync(url).Result)
+                {
+                    responseBody = response.Content.ReadAsStringAsync().Result;
+                    responseBody = System.Text.RegularExpressions.Regex.Unescape(responseBody);
+                }
+            }
+            return responseBody;
+        }
+    }
+}
+
---
+++ SlackExport/Dto/FileDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackExport.Dto
+{
+    public class FileDto
+    {
+        public string channnelName { get; set; }
+        public string filePath { get; set; }
+    }
+}
